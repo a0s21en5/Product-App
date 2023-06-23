f948db71130b31c94df49620fdd9488a903e5296
Ashish Sahu <ashsahu9713@gmail.com>
2023-06-23 18:40:15 +0530

    Update

---
+++ Employee_Web_Application/Models/Employees.cs
@@ -0,0 +1,11 @@
+﻿namespace Employee_Web_Application.Models
+{
+    public class Employees
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string EmployeeEmail { get; set; }
+        public string EmployeePassword { get; set; }
+        public string EmployeeSalary { get; set; }
+    }
+}
---
+++ Employee_Web_Application/Repository/Interface/IEmployeesRepository.cs
@@ -0,0 +1,11 @@
+﻿using Employee_Web_Application.Models;
+
+namespace Employee_Web_Application.Repository.Interface
+{
+    public interface IEmployeesRepository
+    {
+        Task<List<Employees>> GetAllEmployees();
+        Task<Employees> GetEmployeesByName(string employeeName);
+        Task<bool> InsertEmployees(Employees employees);
+    }
+}
